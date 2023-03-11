165f0f71aacfd11d6204c6bc2a2e751c05472d76
RAJSABARI K P <rajsabari74@gmail.com>
2023-03-11 15:45:15 +0530

    java

---
+++ OOP1/src/OOPS1.java
@@ -0,0 +1,13 @@
+public class OOPS1 {
+    public static void main(String[] args){
+       OOPS2 car1= new OOPS2("raj",52);
+//       car1.name="jhd";
+//       car1.price=2332;
+        System.out.println(car1.name+car1.price);
+//        OOPS2 car2= new OOPS2();
+//        car2.name="bmw";
+//        car2.price=100;
+//        System.out.println(car2.name+car2.price);
+
+    }
+}
---
+++ OOP1/src/OOPS2.java
@@ -0,0 +1,10 @@
+public class OOPS2 {
+    String name;
+    int price;
+OOPS2(String name, int price){
+    this.name=name;
+    this.price=price;
+
+}
+
+}
